7fb618018cf5e90a2fdbc09756962d33f6a0cead
Ethan Manarin <35640579+kindanon@users.noreply.github.com>
2019-10-17 12:36:05 -0400

    reformat code, add reddit bot

---
+++ PlanetBirthdays-reddit.py
@@ -0,0 +1,24 @@
+import praw
+
+#will scan every 5 minutes
+#probably need to fix earth birthday timer
+
+#do login function on start
+#can either cron the execution
+#or create a sleep for 5 minutes
+
+#if time now is >= top of list, 
+#probably do it by day
+#happy x bday <planet>
+#delete top of PlanetBirthdays.txt
+
+#"🎂 Happy " + + " Birthday " + + "!"  <-- title
+#see you again at <date> <-- body text
+
+#potentially do a message every x revolutions 
+#potentially do a message for partial transits 
+#will probably have multiple text files for different info
+#need to check each one
+#should get pictures of planets as well
+
+#see if i can change subreddit banner each day with party hats
--- README.md
+++ README.md
@@ -1,14 +1,18 @@
 # planet Birthdays
 
 Calculates the birthdays (orbit times) of the planets since 00001-01-01 
-\ \
-since a birthday is defined as 1 orbit acround the sun, it makes sense that the \
-planets are different age 
-\ \
+\ 
+\
+since a birthday is defined as 1 orbit around the sun, it makes sense that the \
+planets age at different rates
+\ 
+\
 run as "python planet-birthdays-term.py" for a list of planet birthdays for the \
 next 10 years + their current age
-\ \
+\ 
+\
 run as "python planet-birthdays-file.py" for a file of all birthdays from now \
 until 9987 (max date before rollover)
-\ \
+\ 
+\
 will eventually power a reddit bot 
